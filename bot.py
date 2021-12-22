9818bbe3064181ce490dd6544b35fb50ef13dfb9
Ricardo Gabriel Ramírez Arriaga <a01338143@itesm.mx>
2021-12-22 14:12:21 -0600

    embed message layout

--- bot.py
+++ bot.py
@@ -1,4 +1,5 @@
 import discord
+from discord.colour import Color
 import riotApi
 from discord.ext import commands
 import utils
@@ -18,11 +19,14 @@ async def on_ready():
 async def whois(ctx, region, id):
     print('command whois called')
     res = await riotApi.getSummoner(region, id)
-    embed = discord.Embed()
+    embed = discord.Embed(color=0x71f79f)
     embed.set_author(name=res.name, url=res.opgg_url,
                      icon_url=res.profile_icon)
-    embed.title = f'{res.tier} {res.rank}'
-    embed.add_field(name=res.name, value=res.rank, inline=False)
+    embed.title = f'{res.tier.capitalize()} {res.rank}'
+    embed.description = f'Level: {res.level} - LP: {res.lp}'
+    embed.add_field(name='WR', value=f'{res.wr}%', inline=True)
+    embed.add_field(name='Wins', value=res.wins, inline=True)
+    embed.add_field(name='Losses', value=res.losses, inline=True)
     embed.set_thumbnail(url=res.emblem)
     await ctx.send(embed=embed)
 
